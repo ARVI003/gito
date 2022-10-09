28d229035c092d6a97e2f3864ba8bf7e9a4dcf9b
ARVI003 <nomanovomurzak7@gmail.com>
2022-10-09 11:47:33 +0600

    yhfud

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+const  swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
